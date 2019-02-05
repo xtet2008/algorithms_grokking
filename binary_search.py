ee9066e21cecf951a6763e6d63cb3a8e54104c3e
张胜 <zhangsheng@192.168.0.106>
2019-02-05 19:59:21 +0800

    add binary_search

---
+++ binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(list, item):
+    low = 0
+    high = list.__len__() - 1
+
+    while low <= high:
+        mid = (low + high)/2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        elif guess < item:
+            low = mid + 1
+        elif guess > item:
+            high = mid -1
+    return None
+
+
+my_list = [1, 3, 5, 7, 9]
+print (binary_search(my_list, 3))
+print (binary_search(my_list, 4))
